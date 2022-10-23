433d3a58b09504ac2915f3038cae9213d0364d8d
SkadiAegis <95361790+SkadiAegis@users.noreply.github.com>
2022-10-23 12:34:14 +0200

    Ejercicio 01 de la practica P3

--- P3Genericos/E01/src/Main.java
+++ P3Genericos/E01/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<String> list = new LinkedList<>();
+
+        Stack.add(list, "Hola");
+        Stack.add(list, "Adios");
+        Stack.add(list, "Pepe");
+        Stack.add(list, "Juan");
+        Stack.add(list, "Maria");
+
+
+
+        if (Stack.isEmpty(list)) {
+            System.out.println("List is empty");
+        } else {
+            System.out.println("List is not empty");
+        }
+
+        System.out.println(Stack.removeFirst(list));
+
+        System.out.println(Stack.getFirst(list));
+
+        System.out.println(Stack.toString(list));
+
     }
 }
--- P3Genericos/E01/src/Stack.java
+++ P3Genericos/E01/src/Stack.java
@@ -1,3 +1,31 @@
-public class Stack {
+import java.util.LinkedList;
 
+
+public class Stack<T> {
+
+
+    private T t; //La T mayuscula es para indicar que tipo de variable es (Int, double, String, etc)
+    public Stack(T t) {
+        this.t=t;
+    } //Constructor que recibe una variable de tipo T y la asigna a la variable t
+
+    public static <T> boolean isEmpty(LinkedList<T> list) {
+        return list.isEmpty();
+    } //Recibe la lista de tipo T y devuelve true si esta vacia y false si no lo esta
+
+    public static <T> T removeFirst(LinkedList<T> list) {
+        return list.removeFirst();
+    } //Recibe la lista de tipo T y devuelve el primer elemento de la lista y lo elimina de la lista
+
+    public static <T> T getFirst(LinkedList<T> list) {
+        return list.getFirst();
+    } //Recibe la lista de tipo T y devuelve el primer elemento de la lista
+
+    public static <T> void add(LinkedList<T> list, T element) {
+        list.add(element);
+    }//Recibe la lista de tipo T y un elemento de tipo T que se guarda en la variable t (se pasa asi Stack.add(nombreDeLaLista, lo que quieres añadir)) y lo añade a la lista
+
+    public static <T> String toString(LinkedList<T> list) {
+        return list.toString();
+    } //Recibe la lista de tipo T y devuelve todos los elementos de la lista en forma de String
 }
